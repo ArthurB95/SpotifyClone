e360351c0387b5f10e9a1478f9441c71b89c08fa
Arthur Bicalho <arthurbic95@gmail.com>
2023-03-18 10:46:16 -0300

    Artist Details Page

---
+++ src/pages/AroundYou.jsx
@@ -0,0 +1,9 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useSelector} from 'react-redux';
+
+import {Error, Loader, SongCard} from '../components';
+
+const CountryTracks = () => <div>CountryTracks</div>;
+
+export default CountryTracks;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "e85a67cbdamsh2e88d90172b054ap1367a6jsn21f4a9830739"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "v1/charts/world" }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({query: (artistId) => `v2/artists/details?artist_id=${artistId}`})
+  }),
+});
+
+export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery, useGetArtistDetailsQuery } = shazamCoreApi;
